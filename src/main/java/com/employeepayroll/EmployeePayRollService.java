250abf84a7cf7908005f4108f4e522024bd53069
prudhvi <prudhvieethakota123@gmail.com>
2021-05-23 16:07:42 +0530

    Added: Welcome Message To Employee PayRoll Service Program

---
+++ src/main/java/com/employeepayroll/EmployeePayRollService.java
@@ -0,0 +1,7 @@
+package com.employeepayroll;
+
+public class EmployeePayRollService {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Employee PayRoll Service Program");
+    }
+}
